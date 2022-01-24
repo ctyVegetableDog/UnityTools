c65c2903b8ee2551fe966a46f18186c40c192825
ctyVegetableDog <821789905@qq.com>
2022-01-24 15:51:49 +0800

    123

---
+++ Singleton/BaseSingleton.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+/// <summary>
+/// 没有monobehavior的单例
+/// </summary>
+public class BaseSingleton<T> where T:new()
+{
+    // 构造函数私有
+    protected BaseSingleton() { }
+    // 唯一实例
+    private static T instance;
+    // 返回唯一实例
+    public static T GetInstance()
+    {
+        if (instance == null)
+        {
+            instance = new T();
+        }
+        return instance;
+    }
+}
